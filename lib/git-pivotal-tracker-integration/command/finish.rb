c3e079cadb00956d931d69fe38a2b601c2d8aa3e
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2014-07-21 14:09:36 -0400

    fix the strippage

--- lib/git-pivotal-tracker-integration/command/finish.rb
+++ lib/git-pivotal-tracker-integration/command/finish.rb
@@ -40,7 +40,7 @@ class GitPivotalTrackerIntegration::Command::Finish < GitPivotalTrackerIntegrati
     github = config.github
 
     pr = github.pull_requests.create(
-      base: ask("What branch should this PR go to (preview / master /?):"),
+      base: ask("What branch should this PR go to (preview / master /?):").strip,
       head: "#{config.github_username}:#{branch_name}",
       title: "Fixing #{branch_name}"
     )
